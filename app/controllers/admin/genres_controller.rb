f9cb30274298acdbf9b8c5b0b0e37fb4133eaafc
EC2 Default User <ec2-user@ip-172-31-9-36.us-west-1.compute.internal>
2022-09-08 15:03:04 +0000

    admin　ジャンル機能実装

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -4,6 +4,30 @@
   end
 
   def edit
-    @books = Book.find(params[:id])
+    @genre = Genre.find(params[:id])
   end
+
+  def create
+    @genre = Genre.new(genre_params)
+      if @genre.save
+      redirect_to admin_genres_path
+      else
+      render :index
+      end
+  end
+
+  def update
+        @genre = Genre.find(params[:id])
+    if @genres.update(books_params)
+    redirect_to edit_admin_genre_path(@genres.id)
+    else
+   render :edit
+    end
+  end
+
+private
+def genre_params
+      params.require(:genre).permit(:name)
+end
+
   end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,25 +1,36 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def new
    @item = Item.new
   end
-  
-　def index
-    @book = Book.new
-    @users = User.all
-    @user = current_user
-　end
-  
-  def show
-    @user=User.find(params[:id])
-    @books = @user.books
+
+  def index
+   @items = Item.all
   end
-  
+
+  def show
+
+  end
+
+    def create
+    @item = Item.new(item_params)
+      if @item.save
+      redirect_to admin_items_path
+      else
+      render :index
+      end
+    end
+
   def edit
     @user=User.find(params[:id])
     if @user != current_user
       redirect_to user_path(current_user)
     end
   end
-  
+
+private
+def item_params
+      params.require(:item).permit(:name,:image,:introduction,:genre_id,:price,:is_active)
+end
+
 end
